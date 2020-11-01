388cc09ba6f558fa6c2472f862604d40a9d8ae39
坂本一樹 <ikki@Tempest.local>
2020-11-01 22:24:34 +0900

    微修正

--- aws/terraform/network.tf
+++ aws/terraform/network.tf
@@ -17,7 +17,7 @@ resource "aws_internet_gateway" "main-gw" {
 
 resource "aws_subnet" "public-subnet01" {
     vpc_id = aws_vpc.main-vpc.id
-    cidr_block = "10.0.1.0/24"
+    cidr_block = var.public-cidr01
     availability_zone = "ap-northeast-1a"
     tags = {
       Name = "${var.env}-public-subnet01"
@@ -26,7 +26,7 @@ resource "aws_subnet" "public-subnet01" {
 
 resource "aws_subnet" "public-subnet02" {
     vpc_id = aws_vpc.main-vpc.id
-    cidr_block = "10.0.2.0/24"
+    cidr_block = var.public-cidr02
     availability_zone = "ap-northeast-1c"
     tags = {
       Name = "${var.env}-public-subnet02"
--- aws/terraform/variable.tf
+++ aws/terraform/variable.tf
@@ -1,5 +1,7 @@
 variable "env" {}
 variable "vpc_cidr" {}
+variable "public-cidr01" {}
+variable "public-cidr02" {}
 variable "cent-id" {
   default = "ami-089a156ea4f52a0a3"
 }
