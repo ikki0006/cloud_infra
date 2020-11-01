08b6cd18582a9ccfd8956c1ebedd3fe010e9046e
坂本一樹 <ikki@Tempest.local>
2020-11-01 18:32:35 +0900

    ec2失敗

--- aws/README.md
+++ aws/README.md
@@ -3,3 +3,7 @@
 terraform.tfvarsをaws直下に用意し、下記を記載
 aws_access_key = "XXXXXXXXXX"
 aws_secret_key = "XXXXXXXXXXXXX"
+
+公開鍵
+鍵を作成する場合はpassword保管庫から
+~/.ssh/aws/sta(prod)-proxy.pubを作成すること。
--- aws/terraform/common/ec2.tf
+++ aws/terraform/common/ec2.tf
@@ -1,15 +1,20 @@
+resource "aws_key_pair" "key_pair" {
+  key_name   = "${var.env}-ssh-key"
+  public_key = file("~/.ssh/aws/${var.env}-proxy.pub")
+}
+
 resource "aws_instance" "reverse-proxy" {
     ami = var.cent-id
     instance_type = "t2.micro"
-    key_name = "${var.env}-proxy"
+    key_name = "${var.env}-ssh-key"
     vpc_security_group_ids = [
       "${aws_security_group.default-security-group.id}"
     ]
-    subnet_id = "${aws_subnet.public-subnet.id}"
+    subnet_id = aws_subnet.public-subnet.id
     associate_public_ip_address = "true"
-    root_block_device = {
+    root_block_device {
       volume_type = "gp2"
-      volume_size = "8"
+      volume_size = "10"
     }
     tags = {
         Name = "${var.env}-proxy01"
